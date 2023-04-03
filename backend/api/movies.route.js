f01d53948356eeaec32439882d5276a9fb90d634
khanhphamK2 <20521453@mg.uit.edu.vn>
2023-04-03 22:19:28 +0700

    edit src

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,6 +1,11 @@
 import express from 'express';
+
 import MoviesController from './movies.controller.js';
+
 const router = express.Router(); // get access to express router
+
 router.route('/').get(MoviesController.apiGetMovies);
+
 router.route('/').get((req, res) => res.send('hello world'));
+
 export default router;
--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,17 @@ import MoviesDAO from "./dao/moviesDAO.js";
 
 async function main() {
     dotenv.config();
+
     const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+
     const port = process.env.PORT || 8000;
+
     try {
         // Connect to the MongoDB cluster
         await client.connect();
+
         await MoviesDAO.injectDB(client);
+
         app.listen(port, () => {
             console.log('Server is running on port: ' + port);
         });
