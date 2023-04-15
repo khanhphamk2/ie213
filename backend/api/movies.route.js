29c6e4939bb7060d21ae5d75b372d62754a11f64
khanhphamK2 <20521453@mg.uit.edu.vn>
2023-04-15 15:01:23 +0700

    API Movie and Review

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -2,10 +2,23 @@ import express from 'express';
 
 import MoviesController from './movies.controller.js';
 
+import ReviewsController from './reviews.controller.js';
+
 const router = express.Router(); // get access to express router
 
 router.route('/').get(MoviesController.apiGetMovies);
 
-router.route('/').get((req, res) => res.send('hello world'));
+router.route("/id/:id").get(MoviesController.apiGetMovieById); // Get properties and reviews of a movie by id
+
+router.route("/ratings").get(MoviesController.apiGetRatings); // Get ratings of all movies
+
+// router review
+router
+    .route('/review')
+    .post(ReviewsController.apiPostReview) // post review
+    .put(ReviewsController.apiUpdateReview) // update review
+    .delete(ReviewsController.apiDeleteReview); // delete review
 
 export default router;
+
+
